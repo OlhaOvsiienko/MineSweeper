03df9fed1b58a61ec983e0b238ab09b5341c2ff8
OlhaOvsiienko <137906412+OlhaOvsiienko@users.noreply.github.com>
2023-10-01 22:53:47 +0300

    Update constants-and-classes.js

--- constants-and-classes.js
+++ constants-and-classes.js
@@ -8,7 +8,7 @@ flagImage.classList.add('flag-image');
 // Игровое поле: размер, количество бомб, фвета цифр в ячейках возле бомб
 const sizeLookup = {
   '9': {totalBombs: 5},
-  '16': {totalBombs: 5},
+  '16': {totalBombs: 25},
   '30': {totalBombs: 160}
 };
 
@@ -59,148 +59,86 @@ const whoIsPlaying = document.getElementById('whoIsPlaying');
 // Класс Ячейки
 
 class BoardCell {
-    hasBomb = false;
-    revealed = false;
-    cellImage;
-    rowIndex;
-    colIndex;
-    htmlCell;
+  hasBomb = false;
+  revealed = false;
+  cellImage;
+  rowIndex;
+  colIndex;
+  htmlCell;
   
-    constructor(rowIndex, colIndex) {
-      this.rowIndex = rowIndex;
-      this.colIndex = colIndex;
-      this.htmlCell = document.createElement("td");
-      this.htmlCell.className = "game-cell"; // Добавляем класс для стилизации
+  constructor(rowIndex, colIndex) {
+    this.rowIndex = rowIndex;
+    this.colIndex = colIndex;
+    this.htmlCell = document.createElement("td");
+    this.htmlCell.className = "game-cell"; // Добавляем класс для стилизации
   
-      this.cellImage = document.createElement("img"); // Создаем элемент изображения для ячейки
-      this.cellImage.src = ""; // По умолчанию не показываем изображение
-      this.htmlCell.appendChild(this.cellImage); // Добавляем изображение в ячейку
+    this.cellImage = document.createElement("img"); // Создаем элемент изображения для ячейки
+    this.cellImage.src = ""; // По умолчанию не показываем изображение
+    this.htmlCell.appendChild(this.cellImage); // Добавляем изображение в ячейку
   
-      this.htmlCell.addEventListener("contextmenu", (event) =>
-        // Предотвращаем стандартное контекстное меню браузера
-        this.handleCellRightClick(event)
-      );
+    this.htmlCell.addEventListener("contextmenu", (event) =>
+    // Предотвращаем стандартное контекстное меню браузера
+    this.handleCellRightClick(event)
+    );
   
-      this.htmlCell.addEventListener("click", (event) => this.handleClick());
+    this.htmlCell.addEventListener("click", (event) => this.handleClick());
+  }
+  
+  handleCellRightClick(event) {
+    event.preventDefault(); // Предотвращаем стандартное контекстное меню браузера
+    
+    if (gameOver || this.revealed) {
+      return; // Если игра завершена или ячейка уже открыта, игнорируем клик правой кнопкой
     }
   
-    handleCellRightClick(event) {
-      event.preventDefault(); // Предотвращаем стандартное контекстное меню браузера
-  
-      if (gameOver || this.revealed) {
-        return; // Если игра завершена или ячейка уже открыта, игнорируем клик правой кнопкой
-      }
-  
-      playerResultContainer.style.display = 'none';        
-      whoIsPlaying.innerHTML = `${playerName} is playing...`; 
-      whoIsPlaying.style.display = 'block'; 
+    showWhoIsPlaying(playerName);
 
-      if (!this.flagged) {
-        // Если ячейка не была отмечена флагом, устанавливаем флаг
-        this.flagged = true;
-        this.cellImage.src = flagImage.src; // Установите изображение флага
-      } else {
-        // Если ячейка уже была отмечена флагом, удаляем флаг
-        this.flagged = false;
-        this.cellImage.src = ""; // Очистите изображение (удалите флаг)
-      }
-  
-      if (isFirstClick) {
-        isFirstClick = false;
-        timerRunning = true; // Устанавливаем флаг, что таймер запущен
-        timerInterval = setInterval(updateTime, 1); // Запуск таймера каждую мск
-      }
-
-      if (remainCells === totalBombs) {
-        openAllBombs();
-        const messageWinDiv = document.getElementById('win-message');
-        const messageWin = `${playerName}, congratulations! You win! You found all the bombs in ${hours}:${minutes}:${seconds}:${milliseconds}`;
-        messageWinDiv.textContent = messageWin;
-        messageWinDiv.style.display = 'block'; // Сделать сообщение о победе видимым
-        whoIsPlaying.style.display = 'none';
-    
-        gameOver = true;
-        updateSmile();
-        time = milliseconds + seconds * 1000 + minutes * 60 * 1000 + hours * 60 * 60 * 1000;
-        console.log(`Time in msec: ${time}`);
-    
-        console.log(`Time of the game: ${hours}:${minutes}:${seconds}:${milliseconds}`);
-        Player.addPlayer(players, playerName, time);
-        bestPlayer = Player.findBestPlayer(players);
-        console.log(`Player ${bestPlayer.playerName} has the best time: ${bestPlayer.bestTime} msec`);
-    
-        // Проверяем, улучшил ли игрок результат
-        if (time <= bestPlayer.bestTime) {
-            saveBestPlayer({ playerName, bestTime: time });
-            localStorageBestPlayer = getBestPlayer();
-            showRecordMessage(bestPlayer, playerName, localStorageBestPlayer);
-        }
-      }
-    }
-  
-    handleClick() {
-      if (gameOver) {
-        return;
-      }
-
-      playerResultContainer.style.display = 'none';
-      whoIsPlaying.innerHTML = `${playerName} is playing...`; 
-      whoIsPlaying.style.display = 'block'; 
-  
-      if (this.hasBomb) {
-        openAllBombs();
-  
-        // Показываем сообщение о проигрыше
-        const messageLoseDiv = document.getElementById('lose-message');
-        const messageLose = `${playerName}, sorry, you lost the game!`;
-        messageLoseDiv.textContent = messageLose;
-        messageLoseDiv.style.display = 'block'; // Сделать сообщение видимым
-        whoIsPlaying.style.display = 'none';
-        
-        // Устанавливаем состояние игры "проиграно"
-        gameOver = true;
-        updateSmile();
-    
+    if (!this.flagged) {
+      // Если ячейка не была отмечена флагом, устанавливаем флаг
+      this.flagged = true;
+      this.cellImage.src = flagImage.src; // Установите изображение флага
     } else {
-        // Если ячейка не имеет бомбы, открываем её
-        openCell(this.rowIndex, this.colIndex);
-      }
-      // Проверяем условие победы после каждого клика
-      if (remainCells === totalBombs) {
-        openAllBombs();
-        const messageWinDiv = document.getElementById('win-message');
-        const messageWin = `${playerName}, congratulations! You win! You found all the bombs in ${hours}:${minutes}:${seconds}:${milliseconds}`;
-        messageWinDiv.textContent = messageWin;
-        messageWinDiv.style.display = 'block'; // Сделать сообщение о победе видимым
-        whoIsPlaying.style.display = 'none';
-  
-        gameOver = true;
-        updateSmile();
-        time = milliseconds + seconds * 1000 + minutes * 60 * 1000 + hours * 60 * 60 * 1000;
-        console.log(`Time in msec: ${time}`);
-        console.log(`Time of the game: ${hours}:${minutes}:${seconds}:${milliseconds}`);
-        
-        Player.addPlayer(players, playerName, time);
-        bestPlayer = Player.findBestPlayer(players);
-        console.log(`Player ${bestPlayer.playerName} has time: ${bestPlayer.bestTime} msec`);
-      
-        if (time <= bestPlayer.bestTime) {
-            saveBestPlayer({ playerName, bestTime: time });
-            console.log(`Рекорд: ${bestPlayer.playerName}, лучшее время: ${bestPlayer.bestTime}`);
-            localStorageBestPlayer = getBestPlayer();
-            console.log(`Локальное хранилище - передан:`);
-            console.log(localStorageBestPlayer);
-            showRecordMessage(bestPlayer, playerName, localStorageBestPlayer);
-        }
+      // Если ячейка уже была отмечена флагом, удаляем флаг
+      this.flagged = false;
+      this.cellImage.src = ""; // Очистите изображение (удалите флаг)
+    }
+    
+    if (isFirstClick) {
+      isFirstClick = false;
+      timerRunning = true; // Устанавливаем флаг, что таймер запущен
+      timerInterval = setInterval(updateTime, 1); // Запуск таймера каждую мск
     }
 
-      // Если это первый клик, запускаем таймер
-      if (isFirstClick) {
-        isFirstClick = false;
-        timerRunning = true; // Устанавливаем флаг, что таймер запущен
-        timerInterval = setInterval(updateTime, 1); // Запуск таймера каждую секунду (1000 миллисекунд)
+    winGame(remainCells);
+  }
+  
+  handleClick() {
+    if (gameOver) {
+      return;
     }
-}
+    
+    showWhoIsPlaying(playerName);
+  
+    if (this.hasBomb) {
+      openAllBombs();    
+      showLoseMessage(playerName); // Показываем сообщение о проигрыше
+      gameOver = true; // Устанавливаем состояние игры "проиграно"
+      updateSmile();
+    } else {
+      // Если ячейка не имеет бомбы, открываем её
+      openCell(this.rowIndex, this.colIndex);
+    }
+    
+    // Проверяем условие победы после каждого клика
+    winGame(remainCells);
+
+    // Если это первый клик, запускаем таймер
+    if (isFirstClick) {
+      isFirstClick = false;
+      timerRunning = true; // Устанавливаем флаг, что таймер запущен
+      timerInterval = setInterval(updateTime, 1); // Запуск таймера каждую мск 
+    }
+  }
 }
 
 // Класс Игрок
@@ -253,7 +191,6 @@ class Player {
                 localBestPlayer = player;
             }
         }
-    
         return localBestPlayer;
     }
 }
